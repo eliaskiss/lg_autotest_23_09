4e3acf98ee652e354e00122d2eea14d4e3876ef5
elias <eliaskiss@hotmail.com>
2023-09-20 15:05:58 +0900

    day1

--- Elias/day_1/lec_pillow/lec_pillow.py
+++ Elias/day_1/lec_pillow/lec_pillow.py
@@ -107,50 +107,50 @@ class Pillow:
     ###############################################################
     # Type Text on image
     ###############################################################
-    def draw_text_on_image(self, img_file_path, xPos, yPos, text, size, color, font_name='arial.ttf'):
-        img = Image.open(img_file_path)
-        draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype(font_name, size)
-        draw.text((xPos, yPos), text, color, font=font)
-        dir = os.path.dirname(img_file_path)  # images/buz.jpg -> images/
-        file_name = os.path.basename(img_file_path)  # buz.jpg
-        path = os.path.join(dir, 'text_' + file_name)
-        img.save(path)  # images/rotate_buz.jpg
-        return path
+    # def draw_text_on_image(self, img_file_path, xPos, yPos, text, size, color, font_name='arial.ttf'):
+    #     img = Image.open(img_file_path)
+    #     draw = ImageDraw.Draw(img)
+    #     font = ImageFont.truetype(font_name, size)
+    #     draw.text((xPos, yPos), text, color, font=font)
+    #     dir = os.path.dirname(img_file_path)  # images/buz.jpg -> images/
+    #     file_name = os.path.basename(img_file_path)  # buz.jpg
+    #     path = os.path.join(dir, 'text_' + file_name)
+    #     img.save(path)  # images/rotate_buz.jpg
+    #     return path
 
     ###############################################################
     # Apply filter
     ###############################################################
-    def apply_filter(self, img_file_path, filter):
-        img = Image.open(img_file_path)
-
-        if filter == FILTER.BLUR:
-            filter = ImageFilter.BLUR
-        elif filter == FILTER.CONTOUR:
-            filter = ImageFilter.CONTOUR
-        elif filter == FILTER.DETAIL:
-            filter = ImageFilter.DETAIL
-        elif filter == FILTER.EDGE_ENHANCE:
-            filter = ImageFilter.EDGE_ENHANCE
-        elif filter == FILTER.EDGE_ENHANCE_MORE:
-            filter = ImageFilter.EDGE_ENHANCE_MORE
-        elif filter == FILTER.EMBOSS:
-            filter = ImageFilter.EMBOSS
-        elif filter == FILTER.FIND_EDGES:
-            filter = ImageFilter.FIND_EDGES
-        elif filter == FILTER.SHARPEN:
-            filter = ImageFilter.SHARPEN
-        elif filter == FILTER.SMOOTH:
-            filter = ImageFilter.SMOOTH
-        elif filter == FILTER.SMOOTH_MORE:
-            filter = ImageFilter.SMOOTH_MORE
-
-        img = img.filter(filter)
-        dir = os.path.dirname(img_file_path)  # images/buz.jpg -> imgaes/
-        file_name = os.path.basename(img_file_path)  # buz.jpg
-        path = os.path.join(dir, filter.name + '_' + file_name)
-        img.save(path)  # images/blur_buz.jpg
-        return path
+    # def apply_filter(self, img_file_path, filter):
+    #     img = Image.open(img_file_path)
+    #
+    #     if filter == FILTER.BLUR:
+    #         filter = ImageFilter.BLUR
+    #     elif filter == FILTER.CONTOUR:
+    #         filter = ImageFilter.CONTOUR
+    #     elif filter == FILTER.DETAIL:
+    #         filter = ImageFilter.DETAIL
+    #     elif filter == FILTER.EDGE_ENHANCE:
+    #         filter = ImageFilter.EDGE_ENHANCE
+    #     elif filter == FILTER.EDGE_ENHANCE_MORE:
+    #         filter = ImageFilter.EDGE_ENHANCE_MORE
+    #     elif filter == FILTER.EMBOSS:
+    #         filter = ImageFilter.EMBOSS
+    #     elif filter == FILTER.FIND_EDGES:
+    #         filter = ImageFilter.FIND_EDGES
+    #     elif filter == FILTER.SHARPEN:
+    #         filter = ImageFilter.SHARPEN
+    #     elif filter == FILTER.SMOOTH:
+    #         filter = ImageFilter.SMOOTH
+    #     elif filter == FILTER.SMOOTH_MORE:
+    #         filter = ImageFilter.SMOOTH_MORE
+    #
+    #     img = img.filter(filter)
+    #     dir = os.path.dirname(img_file_path)  # images/buz.jpg -> imgaes/
+    #     file_name = os.path.basename(img_file_path)  # buz.jpg
+    #     path = os.path.join(dir, filter.name + '_' + file_name)
+    #     img.save(path)  # images/blur_buz.jpg
+    #     return path
 
     ###############################################################
     # 2x2
@@ -187,76 +187,76 @@ if __name__ == '__main__':
     img_file_path = './images/buz.jpg'
     pillow = Pillow()
 
-    ################################################
-    # 이미지 정보출력
-    ################################################
-    ic(pillow.get_info(img_file_path))
+    # ###############################################################
+    # # 이미지 정보출력
+    # ###############################################################
+    # ic(pillow.get_info(img_file_path))
 
-    ###############################################################
-    # 이미지 포맷변경
-    ###############################################################
-    new_image = pillow.convert_format(img_file_path, 'png')
-    img_info = pillow.get_info(new_image)
-    ic(img_info)
+    # ###############################################################
+    # # 이미지 포맷변경
+    # ###############################################################
+    # new_image = pillow.convert_format(img_file_path, 'png')
+    # img_info = pillow.get_info(new_image)
+    # ic(img_info)
 
-    ###############################################################
-    # 썸네일 이미지 만들기
-    ###############################################################
-    new_image = pillow.make_thumbnail(img_file_path)
-    ic(new_image)
-    ic(pillow.get_info(new_image))
+    # ###############################################################
+    # # 썸네일 이미지 만들기
+    # ###############################################################
+    # new_image = pillow.make_thumbnail(img_file_path)
+    # ic(new_image)
+    # ic(pillow.get_info(new_image))
 
-    ###############################################################
-    # 이미지 잘라내기(Crop)
-    ###############################################################
-    new_image = pillow.crop_image(img_file_path, 100, 100, 200, 200)
-    ic(new_image)
-    ic(pillow.get_info(new_image))
+    # ###############################################################
+    # # 이미지 잘라내기(Crop)
+    # ###############################################################
+    # new_image = pillow.crop_image(img_file_path, 100, 100, 200, 200)
+    # ic(new_image)
+    # ic(pillow.get_info(new_image))
 
-    ###############################################################
-    # 이미지 크기변경
-    ###############################################################
-    new_image = pillow.resize_image(img_file_path, 300, 300)
-    ic(new_image)
+    # ###############################################################
+    # # 이미지 크기변경
+    # ###############################################################
+    # new_image = pillow.resize_image(img_file_path, 300, 300)
+    # ic(new_image)
 
-    ###############################################################
-    # 이미지 회전
-    ###############################################################
+    # ###############################################################
+    # # 이미지 회전
+    # ###############################################################
     # new_image = pillow.rotate_image(img_file_path, 90, True)
-    new_image = pillow.rotate_image(img_file_path, 90, False)
-    ic(new_image)
+    # # new_image = pillow.rotate_image(img_file_path, 90, False)
+    # ic(new_image)
 
-    ###############################################################
-    # 이미지에 글쓰기
-    ###############################################################
-    new_image = pillow.draw_text_on_image(img_file_path, 500, 500, 'Hellow World', 30, 'red')
-    ic(new_image)
+    # ###############################################################
+    # # 이미지에 글쓰기
+    # ###############################################################
+    # new_image = pillow.draw_text_on_image(img_file_path, 500, 500, 'Hellow World', 30, 'red')
+    # ic(new_image)
 
-    ###############################################################
-    # 필터 적용
-    ###############################################################
-    filter_list = [FILTER.BLUR, FILTER.CONTOUR, FILTER.DETAIL,
-                   FILTER.EDGE_ENHANCE, FILTER.EDGE_ENHANCE_MORE, FILTER.EMBOSS, FILTER.FIND_EDGES,
-                   FILTER.SHARPEN, FILTER.SMOOTH, FILTER.SMOOTH_MORE]
+    # ###############################################################
+    # # 필터 적용
+    # ###############################################################
+    # filter_list = [FILTER.BLUR, FILTER.CONTOUR, FILTER.DETAIL,
+    #                FILTER.EDGE_ENHANCE, FILTER.EDGE_ENHANCE_MORE, FILTER.EMBOSS, FILTER.FIND_EDGES,
+    #                FILTER.SHARPEN, FILTER.SMOOTH, FILTER.SMOOTH_MORE]
+    #
+    # for filter in filter_list:
+    #     new_image = pillow.apply_filter(img_file_path, filter)
+    #     ic(new_image)
 
-    for filter in filter_list:
-        new_image = pillow.apply_filter(img_file_path, filter)
-        ic(new_image)
-
-    ###############################################################
-    # Merge Image
-    ###############################################################
-    row = 2
-    column = 3
-
-    # 빈 캔버스 이미지 객체 생성
-    merge_image = pillow.merge_create_image(row, column, img_file_path)
-
-    # 각 행/열의 이미지 복사
-    for _row in range(row):  # 0, 1
-        for _column in range(column):  # 0, 1, 2
-            pillow.merge_paste_image(merge_image, _row, _column, img_file_path)
-
-    # Merged된 이미지 파일 저장
-    new_image = pillow.merge_save(merge_image, img_file_path)
-    ic(new_image)
+    # ###############################################################
+    # # Merge Image
+    # ###############################################################
+    # row = 2
+    # column = 3
+    #
+    # # 빈 캔버스 이미지 객체 생성
+    # merge_image = pillow.merge_create_image(row, column, img_file_path)
+    #
+    # # 각 행/열의 이미지 복사
+    # for _row in range(row):  # 0, 1
+    #     for _column in range(column):  # 0, 1, 2
+    #         pillow.merge_paste_image(merge_image, _row, _column, img_file_path)
+    #
+    # # Merged된 이미지 파일 저장
+    # new_image = pillow.merge_save(merge_image, img_file_path)
+    # ic(new_image)

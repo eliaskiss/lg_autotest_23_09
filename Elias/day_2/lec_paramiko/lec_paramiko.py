453036652665c63113a503aaaed9d9331444ec9d
elias <eliaskiss@hotmail.com>
2023-09-21 09:55:29 +0900

    day2

--- Elias/day_2/lec_paramiko/lec_paramiko.py
+++ Elias/day_2/lec_paramiko/lec_paramiko.py
@@ -31,7 +31,9 @@ class MySSH:
     def connect(self, host, user_id, user_password, port=22, timeout=None):
         if self.client is None:
             self.client = SSHClient()
-
+            
+            # 이코드가 없으면 'Server ... not found in known_hosts' 예외발생
+            self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             self.client.connect(hostname=host, port=port, username=user_id, password=user_password,
                                 timeout=timeout)
 
@@ -41,8 +43,31 @@ class MySSH:
             else:
                 return False
 
+    ########################################################
+    # Execute Shell Command
+    ########################################################
+    def exeCommand(self, command, isReturn = False):
+        pass
 
 
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    ssh = MySSH()
+
+    try:
+        if ssh.connect('45.115.155.124', 'elias', 'lguser', timeout=5, port=22):
+            ic('SSH is connected')
+        else:
+            ic('Connection is failed')
+    except Exception as e:
+        message = f'--> Exception is {e} (Line: {sys.exc_info()[-1].tb_lineno})'
+        ic(message)
 
 
 
